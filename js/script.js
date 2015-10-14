861121f76a3ef71fa23a8dbb9166318bb2fe15df
Eric Turner <turner.eric81@gmail.com>
2015-10-14 11:47:28 -0400

    personal-site
    Finished version of portfolio site

---
+++ js/script.js
@@ -0,0 +1,218 @@
+/*jshint jquery:true */
+/*global $:true */
+
+var $ = jQuery.noConflict();
+
+$(document).ready(function($) {
+	"use strict";
+
+	/* global google: false */
+
+	/*-------------------------------------------------*/
+	/* =  portfolio isotope
+	/*-------------------------------------------------*/
+
+	var winDow = $(window);
+		// Needed variables
+		var $container=$('.portfolio-box, .masonry-blog');
+		var $filter=$('.filter');
+
+		try{
+			$container.imagesLoaded( function(){
+				$container.trigger('resize');
+				$container.isotope({
+					filter:'*',
+					layoutMode:'masonry',
+					animationOptions:{
+						duration:750,
+						easing:'linear'
+					}
+				});
+			});
+		} catch(err) {
+		}
+
+		winDow.bind('resize', function(){
+			var selector = $filter.find('a.active').attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+			}
+			return false;
+		});
+		
+		// Isotope Filter 
+		$filter.find('a').click(function(){
+			var selector = $(this).attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+
+			}
+			return false;
+		});
+
+
+	var filterItemA	= $('.filter li a');
+
+		filterItemA.on('click', function(){
+			var $this = $(this);
+			if ( !$this.hasClass('active')) {
+				filterItemA.removeClass('active');
+				$this.addClass('active');
+			}
+		});
+
+	/*-------------------------------------------------*/
+	/* =  browser detect
+	/*-------------------------------------------------*/
+	try {
+		$.browserSelector();
+		// Adds window smooth scroll on chrome.
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	}
+
+
+	/*-------------------------------------------------*/
+	/* =  preloader function
+	/*-------------------------------------------------*/
+	winDow.load( function(){
+		var mainDiv = $('#container'),
+			preloader = $('.preloader');
+
+			preloader.fadeOut(400, function(){
+				mainDiv.delay(400).addClass('active');
+			});
+	});
+
+	/*-------------------------------------------------*/
+	/* =  flexslider
+	/*-------------------------------------------------*/
+	try {
+
+		var SliderPost = $('.flexslider');
+
+		SliderPost.flexslider({
+			slideshowSpeed: 5000,
+			easing: "swing"
+		});
+	} catch(err) {
+
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Map
+	/* ---------------------------------------------------------------------- */
+	var contact = {"lat":"40.6863585", "lon":"-80.0643379"}; //Change a map coordinate here!
+
+	try {
+		var mapContainer = $('.map');
+		mapContainer.gmap3({
+			action: 'addMarker',
+			marker:{
+				options:{
+					icon : new google.maps.MarkerImage('images/marker.png')
+				}
+			},
+			latLng: [contact.lat, contact.lon],
+			map:{
+				center: [contact.lat, contact.lon],
+				zoom: 15
+				},
+			},
+			{action: 'setOptions', args:[{scrollwheel:false}]}
+		);
+	} catch(err) {
+
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	magnific-popup
+	/* ---------------------------------------------------------------------- */
+
+	try {
+		// Example with multiple objects
+		$('.zoom').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			}
+		});
+	} catch(err) {
+
+	}
+
+	/*-------------------------------------------------*/
+	/* = skills animate
+	/*-------------------------------------------------*/
+
+	try{
+
+		var skillBar = $('.skills-progress');
+		skillBar.appear(function() {
+
+			var animateElement = $(".meter > p");
+			animateElement.each(function() {
+				$(this)
+					.data("origWidth", $(this).width())
+					.width(0)
+					.animate({
+						width: $(this).data("origWidth")
+					}, 1200);
+			});
+
+		});
+	} catch(err) {
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Form
+	/* ---------------------------------------------------------------------- */
+
+	var submitContact = $('#submit_contact'),
+		message = $('#msg');
+
+	submitContact.on('click', function(e){
+		e.preventDefault();
+
+		var $this = $(this);
+		
+		$.ajax({
+			type: "POST",
+			url: 'contact.php',
+			dataType: 'json',
+			cache: false,
+			data: $('#contact-form').serialize(),
+			success: function(data) {
+
+				if(data.info !== 'error'){
+					$this.parents('form').find('input[type=text],textarea,select').filter(':visible').val('');
+					message.hide().removeClass('success').removeClass('error').addClass('success').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				} else {
+					message.hide().removeClass('success').removeClass('error').addClass('error').html(data.msg).fadeIn('slow').delay(5000).fadeOut('slow');
+				}
+			}
+		});
+	});
+
+});
